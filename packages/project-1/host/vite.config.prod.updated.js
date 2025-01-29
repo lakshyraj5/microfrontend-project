1a325d9fe693cf58fedf937d6d15750750cf7187
lakshyraj5 <lakshyraj8chouhan@gmail.com>
2025-01-29 11:25:35 +0530

    w

---
+++ packages/project-1/host/vite.config.prod.updated.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import federation from '@originjs/vite-plugin-federation';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'app',
+      remotes: {
+        remoteApp: 'https://your-remote-project-url.vercel.app/assets/remoteEntry.js', // Update this to your production URL
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
